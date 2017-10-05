8813771fa72c82840daf5facd7cf4c1d5f6eeb56
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-10-05 10:00:45 +0200

    Første Opgave..

--- src/MadsTheCaptain/MainTheCap.java
+++ src/MadsTheCaptain/MainTheCap.java
@@ -1,8 +1,30 @@
 package MadsTheCaptain;
 
+import java.util.Scanner;
+
 public class MainTheCap {
 
     public static void main(String[] args) {
-        System.out.println("Test2");
+        System.out.println("Normie REEEEEEEEEEEE");
+        //int in; Declare
+        //in = 0; initialize
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int tal = input.nextInt();
+
+
+        if (tal%5==0){
+            System.out.println("HiFive");
+
+        }else if (tal%2==0){
+            System.out.println("HiEven");
+
+        }else{
+            System.out.println("Failure! Try again!");
+        }
+
+
+
     }
 }
