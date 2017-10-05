1873a5fee72d66372d20e13168c7c412085c7071
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-10-05 09:14:41 +0200

    Tester Commit

--- src/MadsTheCaptain/MainTheCap.java
+++ src/MadsTheCaptain/MainTheCap.java
@@ -3,6 +3,6 @@ package MadsTheCaptain;
 public class MainTheCap {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test2");
     }
 }
