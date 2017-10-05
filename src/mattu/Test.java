e11fe573685ecc27434057d6e6665ea9efd28104
Meblom <xuniqh@gmail.com>
2017-10-05 10:26:32 +0200

    HiFive / HiEven

--- src/mattu/Test.java
+++ src/mattu/Test.java
@@ -1,7 +1,31 @@
 package mattu;
 
+// Write a program that prompts the user to enter an integer.
+// If the number is a multiple of 5, print HiFive.
+// If the number is divisible by 2, print HiEven.
+
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Aloha");
+
+        Scanner input = new Scanner(System.in); // Input fra user.
+        System.out.println("Indtast et tal: ");
+        int number1 = input.nextInt();
+
+
+        if (number1 % 5 == 0) {
+            System.out.println("HiFive");
+
+
+        }
+        else if (number1 % 2 == 0){
+            System.out.println("HiEven");
+        }
+
+        else {
+            System.out.println("HiNothing");
+        }
+
     }
 }
