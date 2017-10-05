f40ee402c7a28bea94ca04d4b68d09cd53b4a13c
Michael <mich279q@edu.easj.dk>
2017-10-05 09:50:08 +0200

    Package SaneStreet created. Re-Commit.

--- src/SaneStreet/Main.java
+++ src/SaneStreet/Main.java
@@ -2,6 +2,6 @@ package SaneStreet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Have you heard the word of our lord and saviour, Jesus Christ?");
+        System.out.println("The world is a playground.");
     }
 }
