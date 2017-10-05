ccc6eb6dc913e97d85ff7c0347b729a46ac0ea37
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-10-05 13:44:32 +0200

    Dagens arbejde

--- src/thom9521/HiFive.java
+++ src/thom9521/HiFive.java
@@ -10,16 +10,24 @@ public class HiFive {
 
         int number = input.nextInt();
 
+        //Jeg har lavet en "if" som gør at, hvis mit input tal går op i 5 så bliver der skrevet HiFive
         if(number % 5 == 0) {
             System.out.println("HiFive");
         }
 
+        //Jeg har lavet en "if" som gør at, hvis mit input går op i 2 så bliver der skrevet HiEven
             if(number % 2 == 0){
                 System.out.println("HiEven");
         }
 
-        else {
+        //Hvis ingen af mine "if" er gode så bliver der skrevet HiNothing
+        if(number % 2 != 0 && number % 5 != 0) {
                 System.out.println("HiNothing");
+            }
+
+            if (number == 0){
+                System.out.println("Føj");
+
             }
     }
 }
