a10bb0daea782d24d45ea8ca3e51d6b3af488f73
Benjamin <benjamin@smoerfugl.dk>
2017-10-05 13:44:02 +0200

    sidste ændring

---
+++ src/com/ushulabubko/NestedLoops.java
@@ -0,0 +1,10 @@
+package com.ushulabubko;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 5; i = i + i) {
+
+        }
+    }
+}
